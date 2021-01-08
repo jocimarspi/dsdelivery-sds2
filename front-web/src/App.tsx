a71ae34fa3b6cf4dae0bd7e20c1fc3b0ab2271b0
Jocimar B. A. Huss <jocimarspi@gmail.com>
2021-01-08 20:20:16 -0300

    Create front-web initial files

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Counter from './Counter';
 
 function App() {
   return (
     <div>
-      <Counter />
+      <h1>Hello!</h1>
     </div>
   );
 }
